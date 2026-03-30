262f255e02f738d52bcb5c107678eb2d5a77326e
zero_Marty <osisovsky@gmail.com>
2026-03-30 11:44:26 +0200

    kod

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Globalization;
+using System.Data.SQLite;
 
 namespace Diar
 {
@@ -19,10 +20,22 @@ namespace Diar
             InitializeComponent();
         }
 
+        public SQLiteConnection conn;
+        public SQLiteCommand cmd;
+        public string sql;
+
         private void Form1_Load(object sender, EventArgs e)
         {
             Application.DoEvents();
-            if(!file.)
+            if(!File.Exists("databaze.sqlite")) {
+                SQLiteConnection.CreateFile("databaze.sqlite");
+                conn = new SQLiteConnection("Data Source= databaze.sqlite;version=3;");
+                conn.Open();
+                sql = "CREATE TABLE udalosti (id INT PRIMARY KEY, nazev STRING, interval STRING, priorita STRING, datum DATETIME)";
+                cmd = new SQLiteCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
         }
 
         private void btn_Pridej_Click(object sender, EventArgs e)
@@ -38,6 +51,29 @@ namespace Diar
         private void btn_Delete_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private int hledaniid()
+        {
+            conn.Open();
+            sql = "SELECT * FROM udalosti ORDER BY id";
+            cmd = new SQLiteCommand(sql, conn);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            int hledani = 1;
+            while (reader.Read())
+            {
+                int idted = reader.GetInt32(0);
+                if (hledani != idted)
+                {
+                    reader.Close();
+                    conn.Close();
+                    return hledani;
+                }
+                hledani++;
+            }
+            reader.Close();
+            conn.Close();
+            return hledani;
         }
     }
 }
